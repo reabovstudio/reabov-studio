3e56425ba266e76098ff1ab55064e32ce9b6ee5b
DionisPascaru <denis.pascaru95@gmail.com>
2022-08-31 19:03:41 +0300

    Add scroll horizontal functionality

--- src/components/main/AboutComponent.vue
+++ src/components/main/AboutComponent.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="about container">
-        <h1>About</h1>
-        <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/cG3-8UZCN28" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/cG3-8UZCN28" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </template>
 
--- src/components/main/CarouselComponent.vue
+++ src/components/main/CarouselComponent.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="section-track">
-        <div class="track-item">
+    <div class="horizontal-scroll" ref="horizontalContainer">
+        <div class="scroll-item">
             <banner-component></banner-component>
         </div>
-        <div class="track-item">
+        <div class="scroll-item">
             <about-component></about-component>
         </div>
     </div>
@@ -18,19 +18,68 @@ export default {
     components: {
         BannerComponent,
         AboutComponent
+    },
+    data(){
+        return {
+            scrollCount: 0 
+        }
+    },
+    computed: {
+        el(){
+            return this.$refs.horizontalContainer;
+        },
+    },
+    mounted(){
+        this.preparScrollItems(this.el.children); 
+
+        window.addEventListener('resize', () => {
+            this.el.style.transition = 'none';
+            setTimeout(() => {
+                this.preparScrollItems(this.el.children);
+                this.scrollHorizontal()
+            }, 100);
+        });           
+
+        document.addEventListener('wheel', (e) => {
+            this.scrollCount += e.deltaY;
+            this.scrollHorizontal();            
+        });
+    },
+    methods: {
+        preparScrollItems(items) {
+            this.el.style.width = window.innerWidth + 'px';
+
+            for (const item of items) {
+                item.style.width = window.innerWidth + 'px';
+            }
+        },
+        scrollHorizontal(){
+            let containerWidth = this.el.scrollWidth - window.innerWidth;
+
+            if(this.scrollCount < 0){
+                this.scrollCount = 0;
+            }
+
+            if(this.scrollCount > containerWidth){
+                this.scrollCount = containerWidth;
+            }
+
+            this.el.style.transform = `translateX(-${this.scrollCount}px)`;
+            this.el.style.transition = '0.5s';
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.section-track {
-    width: 100%;
+.horizontal-scroll {
     display: flex;
     flex-wrap: nowrap;
-    overflow-x: auto;
+    overflow-x: visible;
 
-    .track-item {
-        flex: 0 0 auto;
+    .scroll-item {
+        flex: auto 0 0;
+        width: 100vw;
     }
 }
 </style>
