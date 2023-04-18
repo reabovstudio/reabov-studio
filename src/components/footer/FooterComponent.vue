2b1d2956ff661cd55071507379aa29f6faa77883
Dionis <d.pascaru@developmentaid.org>
2023-04-18 11:12:24 +0300

    Fix visible bugs

--- src/components/footer/FooterComponent.vue
+++ src/components/footer/FooterComponent.vue
@@ -9,7 +9,7 @@
           <el-menu class="navigation-bar-menu"
                    background-color="transparent">
             <el-menu-item index="1" @click="goToSection('AboutRef')">About</el-menu-item>
-            <el-menu-item index="2" @click="goToSection('PortfolioRef')">Portfolio</el-menu-item>
+            <el-menu-item index="2" @click="goToSection('PortfolioRef', 400)">Portfolio</el-menu-item>
 <!--            <el-menu-item index="4" @click="goToSection('StockRef')">Stock</el-menu-item>-->
             <el-menu-item index="3" @click="goToSection('SocialRef')">Social</el-menu-item>
             <el-menu-item index="5" @click="goToSection('ContactRef')">Contact</el-menu-item>
@@ -30,13 +30,17 @@ export default {
     CopyrightComponent
   },
   methods: {
-    goToSection(value) {
-      const top = document.getElementById(value).offsetTop;
-      window.scrollTo({
-        top: top - 132,
-        left: 0,
-        behavior: 'smooth'
-      });
+    goToSection(value, height = 0) {
+      if (this.$route.name !== 'HomeView') {
+        this.$router.push('/');
+      } else {
+        const top = document.getElementById(value).offsetTop;
+        window.scrollTo({
+          top: top - height,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
     },
   }
 }
