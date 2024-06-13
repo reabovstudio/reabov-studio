ebdcd6204817ded4bc6d7ed59f5643ea66107315
reabov <reabovstock@gmail.com>
2024-06-14 01:00:32 +0300

    Add membership plan card

--- src/components/main/membership/MembershipComponent.vue
+++ src/components/main/membership/MembershipComponent.vue
@@ -8,63 +8,111 @@
       <div class="membership-block-title">
         <h1>Pricing</h1>
       </div>
-      <div class="membership-slot">
-        <div class="membership-slot-widget">
-          <div class="slot-circle"></div>
-          <span class="slot-text">Slots available</span>
-        </div>
-      </div>
       <!-- block 1-->
       <div class="membership-item">
+        <div class="membership-slot">
+          <div class="membership-slot-widget">
+            <div class="slot-circle slot-green"></div>
+            <span class="slot-text">Slots available</span>
+          </div>
+        </div>
         <div class="membership-block green">
           <div class="membership-block-image">
             <img src="@/assets/images/membership/basic.gif" alt="basic">
           </div>
           <div class="membership-block-shape">
-            <h2 class="title-shape">Priority 2</h2>
+            <h2 class="title-shape">Basic</h2>
           </div>
           <div class="membership-block-info">
             <ul>
+              <li>Standard Priority</li>
               <li>One Request At a Time</li>
               <li>Average 48 Hour Delivery</li>
-              <li>Unlimited STOCK Images</li>
-              <li>Private Telegram Chat</li>
-              <li>Unlimited Team Members</li>
-              <li>Unlimited STOCK Images</li>
-              <li>Manage tasks with Trello Board</li>
-              <li>Pause or cancel anytime</li>
+              <li class="text-muted">Unlimited Brands</li>
+              <li class="text-muted">Private Telegram Chat</li>
+              <li class="text-muted">Unlimited Team Members</li>
+              <li class="text-muted">Unlimited Stock Images</li>
+              <li class="text-muted">Manage tasks with Trello Board</li>
             </ul>
           </div>
           <div class="membership-block-price">
             <span class="membership-block-price-number">2995 $</span>
-            <span class="membership-block-price-text">per month</span>
+            <span class="membership-block-price-text">one month</span>
+          </div>
+          <div class="membership-block-footer-text">
+            <span>Pause or cancel anytime</span>
           </div>
         </div>
       </div>
       <!-- block 2-->
       <div class="membership-item">
-        <div class="membership-block red">
+        <div class="membership-slot">
+          <div class="membership-slot-widget">
+            <div class="slot-circle slot-green"></div>
+            <span class="slot-text">Slots available</span>
+          </div>
+        </div>
+        <div class="membership-block yellow">
           <div class="membership-block-image">
-            <img src="@/assets/images/membership/pro.gif" alt="pro">
+            <img src="@/assets/images/membership/extended.gif" alt="pro">
           </div>
           <div class="membership-block-shape">
-            <h2 class="title-shape">Priority 1</h2>
+            <h2 class="title-shape">Extended</h2>
           </div>
           <div class="membership-block-info">
             <ul>
-              <li>Double the Requests</li>
-              <li>Average 24 Hour Delivery</li>
-              <li>Unlimited STOCK Images</li>
-              <li>Private Telegram Chat</li>
-              <li>Unlimited Team Members</li>
-              <li>Unlimited STOCK Images</li>
-              <li>Manage tasks with Trello Board</li>
-              <li>Pause or cancel anytime</li>
+              <li>Standard Priority</li>
+              <li>One Request At a Time</li>
+              <li>Average 48 Hour Delivery</li>
+              <li class="text-muted">Unlimited Brands</li>
+              <li class="text-muted">Private Telegram Chat</li>
+              <li class="text-muted">Unlimited Team Members</li>
+              <li class="text-muted">Unlimited Stock Images</li>
+              <li class="text-muted">Manage tasks with Trello Board</li>
             </ul>
           </div>
           <div class="membership-block-price">
             <span class="membership-block-price-number">5995 $</span>
-            <span class="membership-block-price-text">per month</span>
+            <span class="membership-block-price-text">3 month</span>
+          </div>
+          <div class="membership-block-footer-text">
+            <span>Pause or cancel anytime</span>
+          </div>
+        </div>
+      </div>
+      <!-- block 3-->
+      <div class="membership-item">
+        <div class="membership-slot">
+          <div class="membership-slot-widget">
+            <div class="slot-circle slot-red"></div>
+            <span class="slot-text">Slots available</span>
+          </div>
+        </div>
+        <div class="membership-block red">
+          <div class="membership-block-image">
+            <img src="@/assets/images/membership/premium.gif" alt="pro">
+          </div>
+          <div class="membership-block-shape">
+            <h2 class="title-shape">Premium</h2>
+          </div>
+          <div class="membership-block-info">
+            <ul>
+              <li>Standard Priority</li>
+              <li>One Request At a Time</li>
+              <li>Average 24 Hour Delivery</li>
+              <li class="text-muted">Unlimited Brands</li>
+              <li class="text-muted">Private Telegram Chat</li>
+              <li class="text-muted">Unlimited Team Members</li>
+              <li class="text-muted">Unlimited Stock Images</li>
+              <li class="text-muted">Manage tasks with Trello Board</li>
+            </ul>
+          </div>
+          <div class="membership-block-price">
+            <span class="membership-block-price-number">7995 $</span>
+            <span class="membership-block-price-text">one month</span>
+          </div>
+          <div class="membership-block-footer-text">
+            <span>Pause or cancel anytime</span>
           </div>
         </div>
       </div>
