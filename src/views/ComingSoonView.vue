3351b3e101410b7f5cf962e6cf21d31bf98aa604
DionisPascaru <denis.pascaru95@gmail.com>
2024-11-04 22:53:55 +0200

    Create coming soon component

---
+++ src/views/ComingSoonView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="coming-soon">
+    <div class="coming-soon-img">
+      <img src="@/assets/images/pause.svg" alt="pause">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoonView'
+}
+</script>
+
+<style  lang="scss">
+html {
+  zoom: unset!important;
+}
+.coming-soon {
+  background-color: #000;
+
+  &-img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+
+    img {
+      width: 250px;
+
+    }
+  }
+}
+</style>
